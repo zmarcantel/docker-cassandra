bf69258bb7e2632d1e35652a6748d8a1cc86a7b6
Zachary Marcantel <zmarcantel@utexas.edu>
2014-04-02 16:59:07 -0500

    set seeds to local ip if no links

--- init.sh
+++ init.sh
@@ -3,7 +3,11 @@
 IP=`hostname --ip-address`
 SEEDS=`env | grep CASS[0-9]_PORT_9042_TCP_ADDR | sed 's/CASS[0-9]_PORT_9042_TCP_ADDR=//g' | sed -e :a -e N -e 's/\n/,/' -e ta`
 
+if [ -z "$SEEDS" ]; then
+  SEEDS=$IP
+fi
 
+echo "Listening on: "$IP
 echo "Found seeds: "$SEEDS
 
 # Setup Cassandra
